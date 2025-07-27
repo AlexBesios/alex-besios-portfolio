26c499f2897bf3206f6032074168d93af85c9cda
AlexBesios <alexbesios02@gmail.com>
2025-07-27 20:56:26 +0300

    Add public EmailJS config for GitHub Pages deployment

---
+++ config-public.js
@@ -0,0 +1,11 @@
+// Public EmailJS configuration (safe to commit)
+// These are PUBLIC keys that are meant to be exposed in frontend code
+window.emailjsConfig = {
+  serviceId: 'service_sbqiflt',
+  templateId: 'template_vpwstxi', 
+  publicKey: 'ngL6t8cTzK2FWNfUV'
+};
+
+// Note: EmailJS public keys are designed to be exposed in frontend code
+// They only allow sending emails TO your configured email address
+// They cannot be used to access your EmailJS account or send spam FROM your email
